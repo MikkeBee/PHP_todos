51139160d88d9d84f551cc0fe3e7d767f1c3c667
MikkeBee <mjblakley87@gmail.com>
2022-04-08 15:44:25 +0300

    first commit

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php $host = 'db';
+    $dbname = 'blob'; // database name loginapp or 7
+    $dbuser = 'root'; //db username
+    $dbpassword = 'lionPass'; // db pass
+
+    //Two ways 1. PDO and 2.MySQLI
+
+    $connection = new mysqli($host, $dbuser, $dbpassword, $dbname);
+
+    if($connection->connect_error){
+        die("Connection failed: " . $connection->connect_error);
+     }
+
+    // else{
+    //     echo "Connected to MySQL server success";
+    // }
+
+    ?>
