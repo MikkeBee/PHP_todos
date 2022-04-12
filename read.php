132c035619af7ff004ff3ab0b0ebcb01df441ca7
Jonasson Oscar <oscjjonasson@gmail.com>
2022-04-12 21:05:52 +0300

    Re did index, delete works, update doesnt

--- read.php
+++ read.php
@@ -3,19 +3,57 @@ include './db.php';
 
 $query = "SELECT * FROM todos ORDER BY 'date' desc";
 $result = mysqli_query($connection, $query);
-// $row = mysqli_fetch_array($result);
 if(!$result){
-    die('Nothing to get');
+  die('Nothing to get');
 };
+
+
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $task = $_POST['task'];
+    $query = "UPDATE todos SET task='$task' WHERE id = $id";
+  
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+      die("Update query failed" . mysqli_error($connection));
+    }
+  };
+
+if (isset($_POST['delete'])) {  
+    $id = $row['id']; 
+    $query = "DELETE FROM todos WHERE id = $id";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+      die("Delete query failed" . mysqli_error($connection));
+    }
+  };
+
+
 
 
 
 ?>
 
-<div><?php while($row = mysqli_fetch_array($result)){
+<?php while($row = mysqli_fetch_array($result)){
     $id = $row['id'];
     $task = $row['task'];
     $date = $row['date'];
-    echo "<li class='listItem'> <p>ID: $id </p> <p> $task</p> <p> $date</p> </li>";
-}
- ?></div>
+ ?>
+ <div>
+     <form action="delete.php" method="post" >
+     <select name="id" id="">
+         <?= "<option value='$id'>$id</option>" ?>    
+     </select>
+     <input type="submit" name="submit" value="DELETE">
+ </form>
+
+    <form class="listItem" action="update.php" method="post">
+         <select name="id" id="">
+          <?= "<option value='$id'>$id</option>"?>     
+        </select> 
+         <input class="textfield" type="text"  value="<?= $task ?>" name="task" /> 
+          <p> <?=$date?> </p> 
+         <input type="submit" name="update" value="Update">
+    </form>
+</div>
+ <?php } ?>   
